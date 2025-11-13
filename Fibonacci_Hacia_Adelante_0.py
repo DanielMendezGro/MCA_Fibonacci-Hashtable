aaf2c492e1f5cc299838c0a8a00376f2dba92b7b
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 23:30:36 -0600

    Refactor comments in Fibonacci_Hacia_Adelante_0.py
    Updated comments and formatting in Fibonacci_Hacia_Adelante_0.py.

--- Fibonacci_Hacia_Adelante_0.py
+++ Fibonacci_Hacia_Adelante_0.py
@@ -1,13 +1,15 @@
-// Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado V. 2/2.
-// Actividad 2.
+"""
+- Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado V. 2/2.
+- Actividad 2.
 
-// By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
-// Noviembre 12, 2025
-// Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelanteGenerator1.py
-// Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
-// Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
+*By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
+*Noviembre 12, 2025
+*Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelanteGenerator1.py
+*Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
+*Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
 
-// Código 2: fibonacci_haciaAdelanteGenerator1.py
+Código 2: fibonacci_haciaAdelanteGenerator1.py
+"""
 
 from typing import Generator
 
