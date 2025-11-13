91bd869f2214e816e476fd820a8672f8417736ff
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 23:33:10 -0600

    Revise comments and source references in script
    Updated comments and references in the Fibonacci script.

--- Fibonacci_Hacia_Adelante.py
+++ Fibonacci_Hacia_Adelante.py
@@ -6,7 +6,7 @@
 *Noviembre 12, 2025
 *Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
 *Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
-*Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
+*Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler
 
 Código 1: fibonacci_haciaAdelante1.py
 """
