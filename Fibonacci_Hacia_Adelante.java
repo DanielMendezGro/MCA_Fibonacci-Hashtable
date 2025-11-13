8852a9ce258d4f1cfcb58f49b90220244a192388
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 22:52:59 -0600

    Revise comments with author and source links
    Updated author information and references in comments.

--- Fibonacci_Hacia_Adelante.java
+++ Fibonacci_Hacia_Adelante.java
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 // Código 2: fibonacci_haciaAdelanteGenerator1.py
 
 from typing import Generator
-
+    
 def fibGen(n: int) -> Generator[int, None, None]:
     yield 0
     if n > 0:
@@ -35,5 +35,12 @@ if __name__ == "__main__":
         print(i)
 
   /*
-  By: 
+By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
+Noviembre 12, 2025
+
+Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelanteGenerator1.py
+            https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
+            
+Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
+Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler
   */
