2eb8305310776fb4f92c34b7bc7c3a05275dc697
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 22:57:56 -0600

    Update comments with author and reference details
    Added author information and references to the code.

--- Fibonacci_Hacia_Adelante.java
+++ Fibonacci_Hacia_Adelante.java
@@ -1,6 +1,12 @@
 // Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado V. 1/2.
 // Actividad 2.
 
+// By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
+// Noviembre 12, 2025
+// Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
+// Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
+// Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
+
 // Código 1: fibonacci_haciaAdelante1.py
 
 def fibAdel(n: int) -> int:
@@ -18,5 +24,5 @@ if __name__ == "__main__":
 // By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
 // Noviembre 12, 2025
 // Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
-//Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
-//Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
+// Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
+// Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
