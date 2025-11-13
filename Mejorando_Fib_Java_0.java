71b212cc8e7b7921fde2b7028a7ef2e1e71f3af7
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 23:52:09 -0600

    Update comments to reflect code changes

--- Mejorando_Fib_Java_0.java
+++ Mejorando_Fib_Java_0.java
@@ -1,11 +1,11 @@
-// Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado IV. 1/2.
+// Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado IV. 2/2.
 // Actividad 4.
 
 // By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
 // Noviembre 12, 2025
-// Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/FibonacciThreads.java
+// Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/FibonacciThreadsBigInteger.java
 // Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
 // Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler
 
-// Código 1: FibonacciThreads.java
+// Código 2: FibonacciThreadsBigInteger.java
 
