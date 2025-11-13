bc785dffed123674cc0d6670b437e49e53508f2c
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 23:32:37 -0600

    Update comments and references in Mejorando_Fib_Java.java

--- Mejorando_Fib_Java.java
+++ Mejorando_Fib_Java.java
@@ -1,10 +1,10 @@
-// Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado V. 1/2.
+// Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado IV. 1/2.
 // Actividad 4.
 
-*By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
-*Noviembre 12, 2025
-*Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
-*Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
-*Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
+// By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
+// Noviembre 12, 2025
+// Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
+// Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
+// Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler
 
-Código 1: fibonacci_haciaAdelante1.py
+// Código 1: fibonacci_haciaAdelante1.py
