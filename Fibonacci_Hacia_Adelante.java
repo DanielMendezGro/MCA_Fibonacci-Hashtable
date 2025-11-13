e2650588156093e33716596e08d5be3b035193b0
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 22:57:28 -0600

    Refactor comments in Fibonacci_Hacia_Adelante.java
    Updated comments to use single-line syntax and removed unnecessary line breaks.

--- Fibonacci_Hacia_Adelante.java
+++ Fibonacci_Hacia_Adelante.java
@@ -15,10 +15,8 @@ if __name__ == "__main__":
     n = int(input("¿De qué posición quieres el valor de Fibonacci?: "))
     print(f"El  {n}-ésimo valor de Fibonacci es: {fibAdel(n)}") 
 
-/* By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
-Noviembre 12, 2025
-
-Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
-            
-Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
-Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler *//
+// By: Méndez Guerrero Daniel Iván || Ciencias de la Computación - UNAM
+// Noviembre 12, 2025
+// Referencia: https://github.com/RobertoMendezM/MCA1_2026-1/blob/main/Sucesiones/fibonacci_haciaAdelnate1.py
+//Nota: Este código fue modificado de su fuente original para cumplir con los requerimentos solicitados de la actividad.
+//Compilador online utilizado: https://www.onlinegdb.com/online_java_compiler */
