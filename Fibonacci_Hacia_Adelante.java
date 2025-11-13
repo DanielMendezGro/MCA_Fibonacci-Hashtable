eee71945afa63ef467eb5090147052359a1ff6ba
Daniel <daniel.mg@ciencias.unam.mx>
2025-11-12 22:50:33 -0600

    Implement Fibonacci sequence in Java
    Added two implementations of Fibonacci: one using iteration and another using a generator.

--- Fibonacci_Hacia_Adelante.java
+++ Fibonacci_Hacia_Adelante.java
@@ -1,3 +1,39 @@
 // Implementación de los dos códigos a replicar junto a las mejoras requeridas en el apartado V.
 // Actividad 2.
 
+// Código 1: fibonacci_haciaAdelante1.py
+
+def fibAdel(n: int) -> int:
+    if n == 0: return n
+    penultimo: int = 0
+    ultimo:    int = 1
+    for _ in range(1, n):
+        penultimo, ultimo = ultimo, penultimo + ultimo
+    return ultimo
+
+if __name__ == "__main__":
+    n = int(input("¿De qué posición quieres el valor de Fibonacci?: "))
+    print(f"El  {n}-ésimo valor de Fibonacci es: {fibAdel(n)}") 
+
+// Código 2: fibonacci_haciaAdelanteGenerator1.py
+
+from typing import Generator
+
+def fibGen(n: int) -> Generator[int, None, None]:
+    yield 0
+    if n > 0:
+        yield 1
+    penultimo: int = 0
+    ultimo:    int = 1
+    for _ in range(1, n):
+        penultimo, ultimo = ultimo, penultimo + ultimo
+        yield ultimo
+
+if __name__ == "__main__":
+    n = int(input("¿Fibonacci hasta la posición?: "))
+    for i in fibGen(n):    
+        print(i)
+
+  /*
+  By: 
+  */
